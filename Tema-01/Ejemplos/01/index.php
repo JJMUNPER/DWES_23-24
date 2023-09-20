780ba7d2818a3d53037b99af33bf599d981b6b04
JJMUNPER <jmunper007@g.educaand.es>
2023-09-20 14:44:36 +0200

    Primer commit

---
+++ Tema-01/Ejemplos/01/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+Ejemplo 1 - PHP
+
+Hola Mundo
+
+
+*/
+
+echo "<h1>";
+echo "<hr>";
+echo "<center>";
+echo "Hola Mundo PHP ";
+echo "<BR>";
+echo "Soy Juan Jesus";
+echo "</center>";
+echo "</hr>";
+echo "</h1>";
+echo "DWES - Curso 23/24";
+
+
+
+?>
