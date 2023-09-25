0ce9cd02c1b5dec21a5846205099478a01b3e08c
JJMUNPER <jmunper007@g.educaand.es>
2023-09-25 14:43:12 +0200

    Inicio tema2

---
+++ Tema-02/Ejemplos/02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 02 - Tema 2</title>
+</head>
+
+<body>
+    <center>
+        <h2>Ejemplo 2 - Tema 2</h2>
+    </center>
+
+    <h4>
+    <?php
+
+    echo "Hola Mundo"; 
+    echo "<br>";
+    echo "Jota";
+
+    ?>
+</h4>
+</body>
+
+</html>
