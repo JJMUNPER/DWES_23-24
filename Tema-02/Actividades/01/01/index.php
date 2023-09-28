cbc24c9de4e5019fb19bfee48f7d62e36ee805e0
JJMUNPER <jmunper007@g.educaand.es>
2023-09-28 13:33:05 +0200

    Inicio actividad Tema2

---
+++ Tema-02/Actividades/01/01/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    //Autor:Juan Jesus
+    //Curso:2ºDaw
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tema 2</title>
+</head>
+
+<body>
+    <center>
+        <h2>Periodicos españoles</h2>
+    </center>
+
+    
+        <?php
+
+        echo "Lorem ipsum dolor sit amet. Et dolor sapiente et enim exercitationem sed magni voluptatem qui rerum nostrum ex earum cupiditate eos minus officiis et voluptatem labore. Est quasi consectetur quo harum quia aut rerum doloremque ut atque molestiae ad quam praesentium ab omnis dolores.";
+        echo "<br>";
+        ?>
+        <?php echo '<a href="http://www.elpais.es" target="_blank">Enlace a El País</a>';?>
+    
+</body>
+
+</html>
