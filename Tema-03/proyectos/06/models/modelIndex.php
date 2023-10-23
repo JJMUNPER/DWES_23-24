4b11e237643352dcaed29996335fdb263847adb4
JJMUNPER <jmunper007@g.educaand.es>
2023-10-23 14:04:45 +0200

    CoDaPro act 10

---
+++ Tema-03/proyectos/06/models/modelIndex.php
@@ -0,0 +1,58 @@
+<?php
+    /*
+        Modelo: modeloIndex.php
+        Descripción: generamos un array con los datos de los libros (simulando una consulta a una base de datos)
+    */
+    
+    $libros =[
+        [
+            'id'=> 1,
+            'titulo' => 'Paco Pepe y el viaje de su vida',
+            'autor' => 'Vikram Vaswani',
+            'genero' => 'Aventura',
+            'precio' => 12.99
+        ],
+        [
+            'id'=> 2,
+            'titulo' => 'Juanito Valderrama y su caballo Guadarrama',
+            'autor' => 'Manuel Loera',
+            'genero' => 'Comedia',
+            'precio' => 29.99
+        ],
+        [
+            'id'=> 3,
+            'titulo' => 'PHP 2: Ahora es personal',
+            'autor' => 'Paco Candela',
+            'genero' => 'Programación',
+            'precio' => 39.99
+        ],
+        [
+            'id'=> 4,
+            'titulo' => 'El Viajero Ubriqueño',
+            'autor' => 'Turronero',
+            'genero' => 'Superación',
+            'precio' => 249.99
+        ],
+        [
+            'id'=> 5,
+            'titulo' => 'JavaScript 2.0',
+            'autor' => 'Alan Gallego',
+            'genero' => 'Programación',
+            'precio' => 22.99
+        ],
+        [
+            'id'=> 6,
+            'titulo' => 'Victor, ¿si, o no?',
+            'autor' => 'JuanMaria',
+            'genero' => 'Suspense',
+            'precio' => 19.99
+        ],
+        [
+            'id'=> 7,
+            'titulo' => 'PHP y MariaDB',
+            'autor' => 'Juan Carlos Moreno',
+            'genero' => 'Programación',
+            'precio' => 5.99
+        ]
+    ];    
+?>
---
+++ Tema-03/proyectos/06/views/viewMostrar.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php include 'views/layouts/head.html' ?>
+    <title>Proyecto 3.1 - Gestión de libros</title>
+</head>
+
+<body>
+    <!-- Capa principal -->
+    <div class="container">
+
+        <!-- cabecera documento -->
+        <header class="pb-3 mb-4 border-bottom">
+            <i class="bi bi-calculator"></i>
+            <span class="fs-6">Proyecto 3.1 - Gestión de libros</span>
+        </header>
+
+        <legend>Detalles del libro</legend>
+
+        <!-- Formulario Nuevo Libro -->
+        <form action="mostrar.php">
+            <!-- id -->
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Id</label>
+                <input type="text" class="form-control" name="id" value="<?=$libro['id']?>" disabled>
+                <!-- <div class="form-text">Introduzca identificador del libro</div> -->
+            </div>
+            <!-- Título -->
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Título</label>
+                <input type="text" class="form-control" name="titulo" value="<?=$libro['titulo']?>" disabled>
+                <!-- <div class="form-text">Introduzca título libro existente</div> -->
+            </div>
+            <!-- Autor -->
+            <div class="mb-3">
+                <label for="autor" class="form-label">Autor</label>
+                <input type="text" class="form-control" name="autor" value="<?=$libro['autor']?>" disabled>
+                <!-- <div class="form-text">Introduzca Autor del libro</div> -->
+            </div>
+            <!-- Género -->
+            <div class="mb-3">
+                <label for="genero" class="form-label">Género</label>
+                <input type="text" class="form-control" name="genero" value="<?=$libro['genero']?>" disabled>
+                <!-- <div class="form-text">Género del libro</div> -->
+            </div>
+            <!-- Precio -->
+            <div class="mb-3">
+                <label for="precio" class="form-label">Precio (€)</label>
+                <input type="number" class="form-control" name="precio" value="<?=$libro['precio']?>" disabled>
+                <!-- <div class="form-text">Introduzca Precio</div> -->
+            </div>
+
+
+            <a class="btn btn-secondary" href="index.php" role="button">volver</a>
+
+        </form>
+
+        <br>
+        <br>
+        <br>
+
+
+
+
+        <!-- Pié del documento -->
+        <?php include 'views/layouts/footer.html' ?>
+
+    </div>
+
+    <!-- javascript bootstrap 532 -->
+    <?php include 'views/layouts/javascript.html' ?>
+</body>
+
+</html>
