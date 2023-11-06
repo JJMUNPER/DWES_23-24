71ced28d0e38331c4c6c94062e178ffb95a2932b
JJMUNPER <jmunper007@g.educaand.es>
2023-11-06 17:23:01 +0100

    Comienzo act 4.1

---
+++ Tema-04/Actividades/01/class/class.calculadora.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * 
+ * Clase Calculadora
+ * 
+ */
+
+ class Calculadora {
+
+    private $valor1;
+    private $valor2;
+    private $operacion;
+    private $resultado;
+
+
+    //Constructor
+
+    public function __construct(
+        $valor1=null, 
+        $valor2=null, 
+        $operacion=null, 
+        $resultado=null
+        ) {
+            $this->valor1 = $valor1;
+            $this->valor2 = $valor2;
+            $this->operacion = $operacion;
+            $this->resultado = $resultado;
+        }
+
+
+        //Metodos
+
+        //Metodo suma
+
+        public function sumarValor(){
+
+            $this ->resultado=$this ->valor1 + $this ->valor2;
+
+        }
+
+        //Metodo resta
+        public function restarValor(){
+
+            $this ->resultado=$this ->valor1 - $this ->valor2;
+        }
+
+        //Metodo multiplicacion
+
+        public function multiplicaValor(){
+
+            $this ->resultado=$this ->valor1 * $this ->valor2;
+        }
+
+        //Metodo division
+
+        public function dividirValor(){
+
+            $this ->resultado=$this ->valor1 / $this ->valor2;
+        }
+
+        //Metodo potencia
+
+        public function potenciaValor(){
+
+            $this ->resultado=pow($this ->valor1, $this ->valor2);
+        }
+
+        //Set y Get de los campos de la clase
+ 
+	/**
+	 * @return mixed
+	 */
+	public function getValor1() {
+		return $this->valor1;
+	}
+	
+	/**
+	 * @param mixed $valor1 
+	 * @return self
+	 */
+	public function setValor1($valor1): self {
+		$this->valor1 = $valor1;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getValor2() {
+		return $this->valor2;
+	}
+	
+	/**
+	 * @param mixed $valor2 
+	 * @return self
+	 */
+	public function setValor2($valor2): self {
+		$this->valor2 = $valor2;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getOperacion() {
+		return $this->operacion;
+	}
+	
+	/**
+	 * @param mixed $operacion 
+	 * @return self
+	 */
+	public function setOperacion($operacion): self {
+		$this->operacion = $operacion;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getResultado() {
+		return $this->resultado;
+	}
+	
+	/**
+	 * @param mixed $resultado 
+	 * @return self
+	 */
+	public function setResultado($resultado): self {
+		$this->resultado = $resultado;
+		return $this;
+	}
+}
+
+?>
