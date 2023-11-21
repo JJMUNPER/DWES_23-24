b4d953653ea27f26c4bf8949e2be1bc503283092
JJMUNPER <jmunper007@g.educaand.es>
2023-11-21 11:08:20 +0100

    Ex T4

--- Examen/Pelis/views/partials/header.php
+++
@@ -1,4 +0,0 @@
-<header class="pb-3 mb-4 border-bottom">
-    <i class="bi bi-bullseye" style="font-size: 30px; color:Green"></i>
-    <span class="fs-4">Proyecto 4.2 CRUD Articulos POO</span>
-</header>
