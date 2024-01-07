5fee8c0021170c5da3d582142855256454e3f37f
JJMUNPER <jmunper007@g.educaand.es>
2024-01-08 00:33:12 +0100

    T6

---
+++ Tema-06/Proyecto/index.php
@@ -0,0 +1,14 @@
+<?php  
+
+require 'libs/database.php';
+require 'libs/controller.php';
+require 'libs/model.php';
+require 'libs/view.php';
+require 'class/class.cliente.php';
+
+require 'libs/app.php';
+require 'config/config.php';
+$app = new App();
+
+
+?>
