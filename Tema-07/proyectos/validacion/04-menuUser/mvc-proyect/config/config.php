3b0b52c5a7a17910a9f3363b695a1103a5ee2336
JJMUNPER <jmunper007@g.educaand.es>
2024-01-23 11:20:43 +0100

    P4T7

---
+++ Tema-07/proyectos/validacion/04-menuUser/mvc-proyect/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/DWES/Tema-07/proyectos/validacion/04-menuUser/mvc-proyect/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'fp');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+?>
