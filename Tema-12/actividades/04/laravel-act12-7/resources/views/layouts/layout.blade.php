c141835be089902e28858352d7a7293d720512da
JJMUNPER <jmunper007@g.educaand.es>
2024-02-27 20:27:01 +0100

    T12

--- Tema-12/actividades/04/laravel-act12-7/resources/views/layouts/layout.blade.php
+++ Tema-12/actividades/04/laravel-act12-7/resources/views/layouts/layout.blade.php
@@ -5,20 +5,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>@yield('titulo') - ONLINE Marroquinería</title>
-  </head>
-  <body>
-    <header>
-      <hgroup>
-          <!-- Titulos y subtitulos -->
-          
-          <div class="container">
-              <h1 class="display-7">@yield('titulo')</h1>
-              <p class="lead">@yield('subtitulo')</p>
-          </div>
+</head>
 
-      </hgroup>
+<body>
+    <header>
+        <hgroup>
+            <!-- Titulos y subtitulos -->
+
+            <div class="container">
+                <h1 class="display-7">@yield('titulo')</h1>
+                <p class="lead">@yield('subtitulo')</p>
+            </div>
+
+        </hgroup>
     </header>
 
     <div class="container">
@@ -27,15 +29,17 @@
 
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
     <!-- Pié de página -->
     <footer class="footer mt-auto py-3 fixed-bottom bg-light">
-      <div class="container">
-      <span class="text-muted">© 2024
-        Juan Jesus Muñoz Perez - DWES - 2º DAW - Curso 22/23</span>
-      </div>
+        <div class="container">
+            <span class="text-muted">© 2024
+                Juan Jesus Muñoz Perez - DWES - 2º DAW - Curso 23/24</span>
+        </div>
     </footer>
-    
-  </body>
+
+</body>
+
 </html>
